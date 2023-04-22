de68893db35d6ee0281f1092d06c176c2289faef
Zoltan Kakuk <sz.zoltan.kakuk@gmail.com>
2023-04-22 01:00:47 -0500

    Fix prod urls

--- apps/ngflix/webpack.prod.config.js
+++ apps/ngflix/webpack.prod.config.js
@@ -2,14 +2,8 @@ const { withModuleFederation } = require('@nrwl/angular/module-federation');
 const config = require('./module-federation.config');
 module.exports = withModuleFederation({
   ...config,
-  /*
-   * Remote overrides for production.
-   * Each entry is a pair of a unique name and the URL where it is deployed.
-   *
-   * e.g.
-   * remotes: [
-   *   ['details', 'https://workspace-ten-sage.vercel.app/'],
-   *   ['account', 'https://ngflix-account.vercel.app/'],
-   * ]
-   */
+  remotes: [
+    ['details', 'https://workspace-ten-sage.vercel.app/'],
+    ['account', 'https://ngflix-account.vercel.app/'],
+  ],
 });
