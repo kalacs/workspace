f7a6e23395ff19703ed581373dccfe0e16e62808
Zoltan Kakuk <sz.zoltan.kakuk@gmail.com>
2023-04-24 12:13:38 -0500

    Fix cors issue finally

--- libs/movies/src/lib/movies.service.ts
+++ libs/movies/src/lib/movies.service.ts
@@ -14,7 +14,7 @@ export class MoviesService {
   constructor(private http: HttpClient) { }
 
   getMoviesByGenres(genreIds: number[]): Observable<Movie> {
-    const url = `${TMDB_BASE_URL}/discover/movie/?api_key=${TMDB_API_KEY}&include_adult=false&include_video=false&page=1&genre_ids=${genreIds.join(',')}`
+    const url = `${TMDB_BASE_URL}/discover/movie?api_key=${TMDB_API_KEY}&include_adult=false&include_video=false&page=1&genre_ids=${genreIds.join(',')}`
     return this.http.get<MovieDataResult>(url).pipe(
       switchMap(data => from(data.results))
     )
