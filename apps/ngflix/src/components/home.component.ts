ebba756d89fc229c790de4430ecde084348b89f2
Zoltan Kakuk <sz.zoltan.kakuk@gmail.com>
2023-04-24 23:09:18 -0500

    Add some styling

--- apps/ngflix/src/components/home.component.ts
+++ apps/ngflix/src/components/home.component.ts
@@ -3,29 +3,21 @@ import { Component } from "@angular/core";
 @Component({
   selector: "kalacsorg-home",
   template: `
-    <main>
-      <section id="categories">
-        <h2>Categories</h2>
-        <ul>
-          <li><a href="#">Action</a></li>
-          <li><a href="#">Comedy</a></li>
-          <li><a href="#">Drama</a></li>
-          <li><a href="#">Horror</a></li>
-          <li><a href="#">Sci-Fi</a></li>
-          <li><a href="#">Thriller</a></li>
-        </ul>
-      </section>
-      
-      <section id="movies">
-        <kalacsorg-category-list></kalacsorg-category-list>
-        <!-- Add more categories and movies as needed -->
-      </section>
-    </main>
-    
-    <footer>
-      <p>&copy; 2023 My Video Streaming Platform</p>
-    </footer>
-  
+    <ul class="hidden text-sm font-medium text-center text-gray-500 divide-x divide-gray-200 rounded-lg shadow sm:flex dark:divide-gray-700 dark:text-gray-400">
+        <li class="w-full">
+            <a href="#" class="inline-block w-full p-4 text-gray-900 bg-gray-100 rounded-l-lg focus:ring-4 focus:ring-blue-300 active focus:outline-none dark:bg-gray-700 dark:text-white" aria-current="page">Action</a>
+        </li>
+        <li class="w-full">
+            <a href="#" class="inline-block w-full p-4 bg-white hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Adventure</a>
+        </li>
+        <li class="w-full">
+            <a href="#" class="inline-block w-full p-4 bg-white hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Comedy</a>
+        </li>
+        <li class="w-full">
+            <a href="#" class="inline-block w-full p-4 bg-white rounded-r-lg hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Family</a>
+        </li>
+    </ul>
+    <kalacsorg-category-list></kalacsorg-category-list>
   `,
 })
 export class HomeComponent {
